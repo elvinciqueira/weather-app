818617821e7fb7053029a202336184c32acb2c9f
elvinciqueira <elvinciqueira@gmail.com>
2019-12-31 21:11:03 -0200

    updating the UI

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,6 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
 cityForm.addEventListener('submit', event => {
   //prevent default action
@@ -10,7 +12,7 @@ cityForm.addEventListener('submit', event => {
 
   //update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
 
@@ -19,4 +21,23 @@ async function updateCity(city) {
   const weather = await getWeather(cityDetails.Key);
 
   return { cityDetails, weather };
+}
+
+function updateUI(data) {
+  const { cityDetails, weather } = data;
+
+  //update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  //remove d-none class if present
+  if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
 }
