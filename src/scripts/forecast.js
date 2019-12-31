24ca1be3acdf64339ef0dce0eb52a7c3a0fb7274
elvinciqueira <elvinciqueira@gmail.com>
2019-12-31 20:28:45 -0200

    get weather api call

--- src/scripts/forecast.js
+++ src/scripts/forecast.js
@@ -1,16 +1,31 @@
 const key = 'feAQPnfkmX1AqHesxGRjAjFn0tEzWPBn';
 
-const getCity = async (city) => {
+getCity('são paulo').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+  console.log(data)
+}).catch(err => console.log(err));
+
+
+  
+//get city information
+async function getCity(city) {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
+  
+  const response = await fetch(base + query);
+  const data = await response.json();
+  
+  return data[0];
+}
+
+//get weather information
+async function getWeather(id) {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
 
   return data[0];
-};
-
-getCity('são paulo')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
-
+}
